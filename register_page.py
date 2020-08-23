f71c9da24a9b39b1d7511d4ed130b4773f850638
Andre amacKenzie <andremckenzie58@gmail.com>
2020-08-22 22:07:35 -0500

    "Implement window elements"

--- register_page.py
+++ register_page.py
@@ -9,9 +9,19 @@ def register_window():
 
     window = tk.Tk()
 
-    
+    window.title("Rock-Paper-Scissors | Registration ")
+    window.geometry("350x350")
+    window.resizable(0, 0)
+    window["background"]="#390009"
 
     fulname = tk.StringVar()
     username = tk.StringVar()
     password = tk.StringVar()
 
+
+    window.mainloop()
+
+
+
+if __name__ == "__main__":
+    register_window()
