75b6ad61241a79ed8620c647347e3a0d1a41bfbd
Andre amacKenzie <andremckenzie58@gmail.com>
2020-08-22 22:06:30 -0500

    "Defined and implimented the registration page and widget string variables"

---
+++ register_page.py
@@ -0,0 +1,17 @@
+# A program that'll allow a user to register with the proram 
+
+import tkinter as tk 
+from tkinter import messagebox
+
+def register_window(): 
+    """Allows for the registration of a usier
+    """
+
+    window = tk.Tk()
+
+    
+
+    fulname = tk.StringVar()
+    username = tk.StringVar()
+    password = tk.StringVar()
+
